f48ebc6d444492f93390452cd068d54cecc6d544
Alex Vogel <avogelm@gmail.com>
2018-11-03 12:58:04 +0100

    scripts de rbac

--- bin/rbac.sh
+++ bin/rbac.sh
@@ -1,15 +1,37 @@
 #!bin/bash
 
+rbac_help=""
+
 #Comprobamos que el numero de parámetros sea correcto
-
-
+if [[ $# -ne "2" ]]; then
+    echo $rbac_help
+    exit -1
+fir
 
 #clean-all
+if [[ $1 -eq "clean-all" ]]; then
+    ./rbac/clean-all.sh
+fi
 
 #reset
+if [[ $1 -eq "reset" ]]; then
+    ./rbac/reset.sh
+fi
 
 #help
+if [[ $1 -eq "help" ]]; then
+    echo $rbac_help
+    exit 1
+fi
 
 #list-commands
+if [[ $1 -eq "list-commands" ]]; then
+    ./rbac/list-commands.sh
+    exit 1
+fi
 
 #request-commands
+if [[ $1 -eq "request-command" ]]; then
+    ./rbac/request-command.sh
+    exit 1
+fi
