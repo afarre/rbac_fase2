49b0e6915ad1dc9eb6310fc1289c45d70f872095
Alex Vogel <avogelm@gmail.com>
2018-11-03 16:03:38 +0100

    rbac help

--- bin/rbac.sh
+++ bin/rbac.sh
@@ -1,37 +1,37 @@
 #!bin/bash
 
-rbac_help=""
-
 #Comprobamos que el numero de parámetros sea correcto
-if [[ $# -ne "2" ]]; then
-    echo $rbac_help
+if [[ $# -lt "3" ]]; then
+    cat ./rbac/help.txt
     exit -1
 fir
 
 #clean-all
 if [[ $1 -eq "clean-all" ]]; then
-    ./rbac/clean-all.sh
+    exec ./rbac/clean-all.sh $2
+    exit 1
 fi
 
 #reset
 if [[ $1 -eq "reset" ]]; then
-    ./rbac/reset.sh
+    exec ./rbac/reset.sh $2
+    exit 1
 fi
 
 #help
 if [[ $1 -eq "help" ]]; then
-    echo $rbac_help
+    cat ./rbac/help.txt
     exit 1
 fi
 
 #list-commands
 if [[ $1 -eq "list-commands" ]]; then
-    ./rbac/list-commands.sh
+    exec ./rbac/list-commands.sh $2
     exit 1
 fi
 
 #request-commands
 if [[ $1 -eq "request-command" ]]; then
-    ./rbac/request-command.sh
+    exec ./rbac/request-command.sh $2
     exit 1
 fi
--- bin/rbac/clean-all.sh
+++ bin/rbac/clean-all.sh
@@ -1,1 +1,13 @@
 #!bin/bash
+
+while true; do
+    read -p "Do you wish to clean all your home and enviroment?(Y/N) " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+#Procedemos a limpiar todo el entorno y home
+
