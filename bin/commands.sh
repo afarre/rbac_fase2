fe04734eb0351a84206f7b4ce5449abb51ce0e6d
Angel Farre <angel_1515@hotmail.es>
2018-11-06 15:11:06 +0000

    Fichero de comandos

---
+++ bin/commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CHROOT='/users/advanced/foo5'
+if [ ! -d $CHROOT ]; then
+	mkdir $CHROOT
+fi
+
+for i in $( ldd $* | grep -v dynamic | cut -d " " -f 3 | sed 's/://' | sort | uniq )
+	do
+		cp --parents $i $CHROOT
+	done
+
+#ARCH amd64
+if [ -f /lib64/ld-linux-x86-64.so.2 ]; then
+	cp --parents /lib64/ld-linux-x86-64.so.2 /$CHROOT
+fi
+
+#ARCH i386
+if [ -f /lib/ld-linux.so.2 ]; then
+	cp --parents /lib/ld-linux.so.2 /$CHROOT
+fi
+
