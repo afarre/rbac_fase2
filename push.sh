e95e0ec103a342b91a1bf08d067822eb381d8ebd
Angel Farre <angel_1515@hotmail.es>
2018-11-06 17:53:39 +0000

    Arreglado script de push y cambios a addUser.sh

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 cp /bin/sshLogin.sh bin/
 cp /bin/commands.sh bin/
 cp /etc/ssh/sshd_config etc/ssh/
-cp /bin/addUser.sh /bin
+cp /bin/addUser.sh bin/
 
 git add -A
 echo "Introduce el mensaje de commit:"; read mensaje
