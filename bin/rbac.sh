bae80b88007cff2c7d631958241b9b0fb57e3f3b
Alex Vogel <avogelm@gmail.com>
2018-11-06 17:24:45 +0000

    something

--- bin/rbac.sh
+++ bin/rbac.sh
@@ -1,37 +1,44 @@
-#!bin/bash
+#!/bin/bash
 
 #Comprobamos que el numero de parámetros sea correcto
-if [[ $# -lt "3" ]]; then
+if [[ $# -ne 1 ]]; then
+    echo "Numero incorrecto de parametros"
     cat ./rbac/help.txt
     exit -1
-fir
+fi
+
+username=`whoami`
 
 #clean-all
-if [[ $1 -eq "clean-all" ]]; then
-    exec ./rbac/clean-all.sh $2
+if [[ "$1" = "clean-all" ]]; then
+    exec ./rbac/clean-all.sh $username
     exit 1
 fi
 
 #reset
-if [[ $1 -eq "reset" ]]; then
-    exec ./rbac/reset.sh $2
+if [[ "$1" = "reset" ]]; then
+    exec ./rbac/reset.sh $username
     exit 1
 fi
 
 #help
-if [[ $1 -eq "help" ]]; then
+if [[ "$1" = "help" ]]; then
+    echo "Has usado el comando de ayuda"
     cat ./rbac/help.txt
     exit 1
 fi
 
 #list-commands
-if [[ $1 -eq "list-commands" ]]; then
-    exec ./rbac/list-commands.sh $2
+if [[ "$1" = "list-commands" ]]; then
+    exec ./rbac/list-commands.sh $username
     exit 1
 fi
 
 #request-commands
-if [[ $1 -eq "request-command" ]]; then
-    exec ./rbac/request-command.sh $2
+if [[ "$1" = "request-command" ]]; then
+    exec ./rbac/request-command.sh $username
     exit 1
 fi
+
+cat ./rbac/help.txt
+exit 1
--- bin/rbac/clean-all.sh
+++ bin/rbac/clean-all.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 while true; do
     read -p "Do you wish to clean all your home and enviroment?(Y/N) " yn
@@ -11,3 +11,28 @@ done
 
 #Procedemos a limpiar todo el entorno y home
 
+
+if groups $1 | grep &>/dev/null '\bbasic\b'; then
+
+	exit 1
+fi
+
+if groups $1 | grep &>/dev/null '\bmedium\b'; then
+
+	exit 1
+fi
+
+if groups $1 | grep &>/dev/null '\badvanced\b'; then
+
+	exit 1
+fi
+
+if groups $1 | grep &>/dev/null '\bdatastore\b'; then
+
+	exit 1
+fi
+
+if groups $1 | grep &>/dev/null '\bvisitor\b'; then
+
+	exit 1
+fi
